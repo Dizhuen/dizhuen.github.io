4db95b399ec453d44544af6996f96a39d32f116c
Dizhuen <dizhuen@gmail.com>
2025-01-28 06:28:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const nav = document.querySelector('.nav');
     const logoLink = document.querySelector('.logo');
 
-    burgerMenu.addEventListener('click', () => {
-        burgerMenu.classList.toggle('active');
-        nav.classList.toggle('open');
-    });
+    if (burgerMenu && nav) { // Проверка, что элементы найдены
+        burgerMenu.addEventListener('click', (e) => {
+            e.preventDefault(); // Добавлено для предотвращения поведения по умолчанию, если button type="submit"
+            burgerMenu.classList.toggle('active');
+            nav.classList.toggle('open');
+        });
+    } else {
+        console.error("Бургер-меню или навигация не найдены!"); // Сообщение об ошибке, если элементы не найдены
+    }
+
 
     const navLinks = document.querySelectorAll('.nav-link');
     navLinks.forEach(link => {
